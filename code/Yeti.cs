901be80e589d9b4bc868aca8044e9e1ef62a853a
yuberee <59583743+yuberee@users.noreply.github.com>
2022-11-22 00:59:16 +0100

    Yeti hammer entity

---
+++ code/Yeti.cs
@@ -0,0 +1,31 @@
+﻿using SandboxEditor;
+
+namespace YetiGame;
+
+[HammerEntity]
+[EditorModel( "models/citizen/citizen.vmdl" )]
+partial class Yeti : AnimatedEntity
+{
+
+	public override void Spawn()
+	{
+
+		SetModel( "models/citizen/citizen.vmdl" );
+		Scale = 1.5f;
+
+	}
+
+	[Event.Tick]
+	public void ComputeAI()
+	{
+
+		// MOVEMENT //
+		
+
+		// ANIMATION //
+
+		var animationHelper = new CitizenAnimationHelper( this );
+
+	}
+
+}
