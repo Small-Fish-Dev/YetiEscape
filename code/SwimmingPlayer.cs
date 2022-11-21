e30cac33bfe7c871526dafd82d19017154abefdf
yuberee <59583743+yuberee@users.noreply.github.com>
2022-11-22 00:17:02 +0100

    First part

---
+++ code/SwimmingPlayer.cs
@@ -0,0 +1,31 @@
+﻿namespace YetiGame;
+
+partial class SwimmingPlayer : AnimatedEntity
+{
+
+	public override void Spawn()
+	{
+
+		SetModel( "models/citizen/citizen.vmdl" );
+
+	}
+
+	public override void Simulate( Client cl )
+	{
+
+		Vector3 direction = new Vector3( Input.Forward, Input.Left, 0 ).Normal;
+
+		Rotation = Rotation.FromYaw( direction.EulerAngles.yaw );
+		Position += direction * 100f * Time.Delta;
+
+	}
+
+	public override void PostCameraSetup( ref CameraSetup setup )
+	{
+
+		setup.Position = Vector3.Up * 800f;
+		setup.Rotation = Rotation.FromPitch( 90f );
+
+	}
+
+}
