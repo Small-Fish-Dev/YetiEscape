53c7efe83c18da2f88cd11436c148d5317c9535e
yuberee <59583743+yuberee@users.noreply.github.com>
2022-11-22 00:34:58 +0100

    Keep the spawn points until we have a map

--- code/YetiEscape.cs
+++ code/YetiEscape.cs
@@ -15,11 +15,23 @@ public partial class YetiEscape : Sandbox.Game
 
 	public override void ClientJoined( Client client )
 	{
-
 		base.ClientJoined( client );
 
 		var pawn = new SwimmingPlayer();
 		client.Pawn = pawn;
 
+		// Get all of the spawnpoints
+		var spawnpoints = Entity.All.OfType<SpawnPoint>();
+
+		// chose a random one
+		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+
+		// if it exists, place the pawn there
+		if ( randomSpawnPoint != null )
+		{
+			var tx = randomSpawnPoint.Transform;
+			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
+			pawn.Transform = tx;
+		}
 	}
 }
